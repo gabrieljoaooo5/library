209b74631a17ed37a59d68d5cb78e9aaedb2542f
João Gabriel Andrade <52970106+gabrieljoaooo5@users.noreply.github.com>
2021-10-28 08:30:11 -0300

    Create model and database

--- djangoLibrary/library/models.py
+++ djangoLibrary/library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    author = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=50)
+    isbn = models.IntegerField()
+    pagesNumber = models.IntegerField()
+    title = models.CharField(max_length=50)
+    publicationYear = models.IntegerField()
+    publisherEmail = models.EmailField()
