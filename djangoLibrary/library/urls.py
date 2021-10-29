d6ef6d3b9beb0cbb3e5a596c3b0c2149e7dc82ef
João Gabriel Andrade <52970106+gabrieljoaooo5@users.noreply.github.com>
2021-10-28 22:17:49 -0300

    Create and edit books

--- djangoLibrary/library/urls.py
+++ djangoLibrary/library/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^book_list/', book_list, name='list_books'),
+    url(r'^book_list/', book_list, name='book_list'),
+    url(r'^book_new/', book_new, name='book_new'),
+    url(r'^book_edit/(?P<pk>[0-9]+)', book_edit, name='book_edit'),
 ]
 
--- djangoLibrary/library/views.py
+++ djangoLibrary/library/views.py
@@ -1,14 +1,34 @@
 from django.forms import ModelForm
 from .models import *
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 
 class BookForm(ModelForm):
-    model = Book
-    fields = ['author', 'publisher', 'isbn', 'pagesNumber', 'title', 'publicationYear', 'publisherEmail']
+    class Meta:
+        model = Book
+        fields = ['author', 'publisher', 'isbn', 'pagesNumber', 'title', 'publicationYear', 'publisherEmail']
 
 def book_list(request, template_name='book_list.html'):
     book = Book.objects.all()
     books = {'list': book}
     return render(request, template_name, books)
+
+def book_new(request, template_name='book_form.html'):
+    form = BookForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, template_name, {'form': form})
+
+
+def book_edit(request, pk, template_name='book_form.html'):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, template_name, {'form': form})
